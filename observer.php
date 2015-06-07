8cf651f2b9c23dd1647f24b7880978fd2a663518
Oddler <oddler@oddler.ru>
2015-06-07 19:54:54 +0400

    Сделал заготовку для нового патерна Observer

---
+++ observer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Oddler
+ * Date: 07.06.15
+ * Time: 19:50
+ */
+
+namespace sofw\core;
+
+/**
+ * soRegistry. Шаблон "Observer".
+ *
+ * @since 1.0
+ */
+
+trait soObserver
+{
+
+}
